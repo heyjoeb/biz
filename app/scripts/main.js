23ef765d7b08cc10cf205ee3b3abc2a9827c42bf
heyjoeb <heyjoeb@hotmail.com>
2016-09-30 13:18:09 -0500

    Added mockjax and mustache

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+/*jshint strict: false */
+/*jshint camelcase: false */
+/* global $ */
+/*eslint-disable no-unused-vars*/
+$(document).ready(function() {
+    // 'use strict';
+    // MENU
+    
+});
